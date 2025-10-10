92d1b12d52cdca2583feabd66caed3433de784b5
ShahinulRafi <shahinul.csecu@gmail.com>
2025-10-10 15:19:34 +0600

    nav, hero, marketing section design done

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,49 +1,61 @@
 // import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import React from "react";
 import { NavLink } from "react-router";
 const Nav = () => {
   return (
-    <nav className="flex justify-between items-center">
+    <nav className="mx-10 mt-10 flex justify-between items-center">
       <NavLink Link to="/home">
         <div class="flex justify-between items-center gap-2">
           <img src="/public/assets/logo.png" className="h-10" alt="" />
-          <h2 className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] font-bold text-xl text-transparent bg-clip-text">Hero.IO</h2>
+          <h2 className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] font-bold text-xl text-transparent bg-clip-text">
+            Hero.IO
+          </h2>
         </div>
       </NavLink>
       <div className="flex gap-5">
         <NavLink
           Link
           to="/home"
-          className={({ isActive }) => (isActive ? "text-[#632EE3] underline font-semibold text-xl" : "font-semibold text-xl")}
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#632EE3] underline font-semibold text-xl"
+              : "font-semibold text-xl"
+          }
         >
           Home
         </NavLink>
         <NavLink
           Link
           to="/apps"
-          className={({ isActive }) => (isActive ? "text-[#632EE3] underline font-semibold text-xl" : "font-semibold text-xl")}
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#632EE3] underline font-semibold text-xl"
+              : "font-semibold text-xl"
+          }
         >
           Apps
         </NavLink>
         <NavLink
           Link
           to="/installation"
-          className={({ isActive }) => (isActive ? "text-[#632EE3] underline font-semibold text-xl" : "font-semibold text-xl")}
-          
+          className={({ isActive }) =>
+            isActive
+              ? "text-[#632EE3] underline font-semibold text-xl"
+              : "font-semibold text-xl"
+          }
         >
           Installation
         </NavLink>
       </div>
-
-      <button>
-        <a href="www.github.com">
-          <FontAwesomeIcon icon={faGithub} />
+      <a href="https://www.github.com">
+        <button className="btn btn-primary flex justify-between items-center gap-1">
+          <FontAwesomeIcon icon={faGithub} className="mt-[1px]"/>
           <h2 className="text-xl font-semibold">Contribute</h2>
-        </a>
-      </button>
+        </button>
+      </a>
     </nav>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import { faAppStoreIos, faGooglePlay } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 const Hero = () => {
-    return (
-        <div>
-            Hero
-            
+  return (
+    <div className="mt-10 mx-10">
+      <h1 className="text-center font-bold text-4xl">We Build</h1>
+      <h1 className="text-center font-bold text-4xl">
+        <span className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-transparent bg-clip-text">
+          Productive
+        </span>{" "}
+        Apps
+      </h1>
+
+      <p className="mt-5 text-center">
+        At HERO.IO, we craft innovative apps designed to make everyday life
+        simpler, smarter, and more exciting. <br /> Our goal is to turn your ideas into
+        digital experiences that truly make an impact.
+      </p>
+      <div className="mt-10 flex justify-center gap-5">
+        <div className="btn btn-outline">
+            <FontAwesomeIcon icon={faGooglePlay} />
+            <h1>Google Play</h1>
         </div>
-    );
+        <div className="btn btn-outline">
+            <FontAwesomeIcon icon={faAppStoreIos} />
+            <h1>App Store</h1>
+        </div>
+      </div>
+      <div className="mt-10 flex justify-center">
+        <img src="/public/assets/hero.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
